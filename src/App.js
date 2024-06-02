c69528dc2eeb93db94cacb9c66184ee24043ac82
asmitt8373 <austinsmittle8373@gmail.com>
2024-06-02 16:45:33 -0700

    setting up file structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react";
+import { BrowserRouter as Router,Route, Routes} from "react-router-dom" ;
+import Home from './Pages/home/homescreen';
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
