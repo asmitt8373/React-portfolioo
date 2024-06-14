18d62caae16bafd788556077aac48ccd91c2d8d6
asmitt8373 <austinsmittle8373@gmail.com>
2024-06-13 18:00:36 -0700

    updated pages and fixed some bugs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import "./App.css";
 import React from "react";
-import { BrowserRouter as Router,Route, Routes} from "react-router-dom" ;
-import Navbar from "./Pages/home/Navbar";
-import Home from './Pages/home/Homescreen/Index';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Pages/Home/Navbar";
+import Home from "./Pages/Home/Homescreen";
+
 function App() {
   return (
     <div className="App">
@@ -10,9 +11,8 @@ function App() {
         <div>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />}>
-            </Route>
-            <Route path="*" element={<div> 404 Not Found</div>}></Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<div>404 Not Found</div>}></Route>
           </Routes>
         </div>
       </Router>
